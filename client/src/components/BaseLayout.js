adacff4de0ab862d994dcc524f8be06f382c183d
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-10-17 19:22:09 -0500

    so it begins

---
+++ client/src/components/BaseLayout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './styling/baseLayout.css';
+
+export default class BaseLayout extends Component {
+  render(){
+  return (
+    <div className="baseLayoutBody">
+      <div className='wordyWords'>
+        This is a whole bunch of words!
+      </div>
+
+
+    </div>
+  )};
+}
