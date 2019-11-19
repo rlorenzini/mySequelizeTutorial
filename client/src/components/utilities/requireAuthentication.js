47a64b00fc7163cc8db54a07acf7ebf94c4b7a45
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-11-19 11:56:14 -0600

    stuff

--- client/src/components/utilities/requireAuthentication.js
+++ client/src/components/utilities/requireAuthentication.js
@@ -1,39 +1,41 @@
-import React, { useState, useEffect } from 'react';
-// import { connect } from 'react-redux';
+import React,{ Component } from 'react';
+import { connect } from 'react-redux';
 
 //this is a High Order Component, meaning any component with requireAuth()
 //will inherit from this component
 //check index.js for components which inherit from this HOC
-export const StateContext = React.createContext();
 
 export default function requireAuth(ComposedComponent) {
-
-  const AuthenticateContext = props => {
-
-    const [username, setUsername] = useState('');
-    const [userid, setUserid] = useState('');
-
-    useEffect(() => {
+  //our function is passing through an entire Component as a variable
+  class Authenticate extends Component {
+    //we are passing props.history, so we must set our constructor with props
+    constructor(props) {
+      super(props)
+      console.log(this.props)
+      //if authenticated is not true, push the page back to the index page
       if(!this.props.isAuthenticated) {
         this.props.history.push('/')
       }
-    }, [this.props.isAuthenticated])
+    };
 
+    componentDidUpdate(nextProps) {
+      if(!nextProps.isAuthenticated) {
+        this.props.history.push('/')
+      }
+    };
+
+    //return the component with the props
+    render() {
       return (
-        <ComposedComponent
-        value={(
-          username, setUsername,
-          userid, setUserid
-        )}
-        {...this.props}/>
+        <ComposedComponent {...this.props}/>
       );
     };
-    // return StateProvider;
   };
-
-  // const mapStateToProps = (state) => {
-  //   return {
-  //     isAuthenticated: state.isAuthenticated
-  //   }
-  // };
-  // connect(mapStateToProps)(AuthenticateContext);
+  //we are using React Redux to pass state between props without a parent/child relation
+  const mapStateToProps = (state) => {
+    return {
+      isAuthenticated: state.isAuthenticated
+    }
+  };
+  return connect(mapStateToProps)(Authenticate);
+};
