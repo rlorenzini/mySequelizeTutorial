b8b5d46117b9d64b0d589a5179980108fb7e9e77
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-11-22 14:16:45 -0600

    update

--- client/src/index.js
+++ client/src/index.js
@@ -21,7 +21,7 @@ import userReducer from './stores/reducers/reducer';
 //=====================OTHER===========================
 import * as serviceWorker from './serviceWorker';
 
-//allReducers is the combined reducer
+//allReducers is the combined reducer 
 
 const store = createStore(
   userReducer,
@@ -29,7 +29,7 @@ const store = createStore(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-); //remove or disable redux devtools extension when in production 
+); //remove or disable redux devtools extension when in production
 
 setAuthenticationHeader(localStorage.getItem('jsonwebtoken'));
 
